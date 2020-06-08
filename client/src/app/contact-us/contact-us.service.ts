6381d07dec2fbf7f20d93a5003f6928ac0fc94c7
abraham <abraham@expertcollective.org>
2020-06-08 08:42:50 -0500

    More work on wiring up API call

--- client/src/app/contact-us/contact-us.service.ts
+++ client/src/app/contact-us/contact-us.service.ts
@@ -1,22 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpResponse,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
-import { ContactInfoDto } from '../dto/contactInfoDto';
+import { ContactInfoDto } from "../dto/contactInfoDto";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ContactUsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  contactInfoResponse$: Observable<any>
+  contactInfoResponse$: Observable<any>;
 
   sendContactInfo(contactData: any) {
-
-    const contactInfo : ContactInfoDto = {
+    const contactInfo: ContactInfoDto = {
       firstName: contactData.firstName,
       lastName: contactData.lastName,
       jobTitle: contactData.jobTitle,
@@ -25,7 +28,7 @@ export class ContactUsService {
       phoneNumber: contactData.phoneNumber,
       emailAddress: contactData.emailAddress,
       commentMessage: contactData.tryingToAchieve,
-    }
+    };
     console.log(
       `[ContactUsService] sendContactInfo contactData: ${JSON.stringify(
         contactInfo,
@@ -33,14 +36,27 @@ export class ContactUsService {
         2
       )}`
     );
-    this.contactInfoResponse$ = this.httpClient.post<any>("api/sendcontact/", contactInfo).pipe(
-      map(data => {
-       console.log(`[ContactUsService] sendContactInfo data: ${JSON.stringify(data, null, 2)}`)
-       return  data
-      }),
-      // catchError(error => {
-      //   return error.message === false || false;
-      // })
-    )
+
+    return this.httpClient
+      .post<any>("http://localhost:8000/api/sendcontact/", {
+        headers: new HttpHeaders().set("Content-Type", "application/json"),
+        params: contactInfo,
+      })
+      .pipe(
+        map((data) => {
+          console.log(
+            `[ContactUsService] sendContactInfo data: ${JSON.stringify(
+              data,
+              null,
+              2
+            )}`
+          );
+          return data;
+        }),
+        catchError((err: HttpErrorResponse) => {
+          console.log(`Logging Interceptor: ${err.error.message}`);
+          return of(new HttpResponse({ body: { message: err.error.message } }));
+        })
+      );
   }
 }
